693d9e91d5ac510c36a7e5b20ed6230e0ce72be5
Franklin Castillo <133930634+francastlee@users.noreply.github.com>
2025-03-13 08:41:17 -0600

    Equipo(DTO, DAL)
    Se desarrolló el DTO y la interfaz con su respectiva implementación de la entidad Equipo.
    
    Co-Authored-By: mceles22 <133931561+mceles22@users.noreply.github.com>

---
+++ AgendaVirtual/BackEnd/DTO/EquipoDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace BackEnd.DTO
+{
+    public class EquipoDTO
+    {
+        public int IdEquipo { get; set; }
+
+        public string Nombre { get; set; } = null!;
+
+        public string? Descripcion { get; set; }
+
+        public DateTime? FechaCreacion { get; set; }
+    }
+}
---
+++ AgendaVirtual/DAL/Implementations/EquipoDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Interfaces;
+using Entities.Entities;
+
+namespace DAL.Implementations
+{
+    public class EquipoDAL : DALGenericoImpl<Equipo>, IEquipoDAL
+    {
+        private AgendaVirtualContext _context;
+        public EquipoDAL(AgendaVirtualContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
