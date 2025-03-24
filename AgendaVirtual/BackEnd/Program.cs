b488488a2968218a333d59f7a6a58cce4005a982
genndy3 <guenndy03@gmail.com>
2025-03-23 22:12:55 -0600

    Authentication: BackEnd

--- AgendaVirtual/BackEnd/Program.cs
+++ AgendaVirtual/BackEnd/Program.cs
@@ -3,8 +3,12 @@ using BackEnd.Services.Interfaces;
 using DAL.Implementations;
 using DAL.Interfaces;
 using Entities.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using System.Diagnostics;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,12 +19,68 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-#region DI
+#region BD
 builder.Services.AddDbContext<AgendaVirtualContext>(optionsAction =>
-                     optionsAction
-                     .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+                    optionsAction
+                    .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 
-     );
+    );
+
+builder.Services.AddDbContext<AuthDBContext>(optionsAction =>
+                    optionsAction
+                    .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+
+    );
+
+#endregion
+
+
+#region Identity
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AuthDBContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 3;
+
+});
+#endregion
+
+#region  JWT
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+
+})
+
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidAudience = builder.Configuration["JWT:ValidAudience"],
+            ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+        };
+    });
+
+
+
+#endregion
+
+#region DI
+
 builder.Services.AddScoped<IUnidadDeTrabajo, UnidadDeTrabajo>();
 
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
@@ -44,6 +104,8 @@ builder.Services.AddScoped<IEventoDAL, EventoDAL>();
 builder.Services.AddScoped<IComentarioService, ComentarioService>();
 builder.Services.AddScoped<IComentarioDAL, ComentarioDAL>();
 
+builder.Services.AddScoped<ITokenService, TokenService>();
+
 #endregion
 
 var app = builder.Build();
@@ -54,19 +116,24 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseAuthentication();
 app.UseMiddleware<ApiKeyMiddleware>();
 app.UseAuthorization();
 
 app.MapControllers();
 
-
-// Esto lo puse para probar la conexión porque aún no hay CRUDs para probar en el swagger, se puede borrar una vez los CRUDs estén hechos :)
-using (var scope = app.Services.CreateScope())
+app.Use(async (context, next) =>
 {
-    var context = scope.ServiceProvider.GetRequiredService<AgendaVirtualContext>();
-    context.Database.EnsureCreated(); // Crea la base de datos si no existe
-    Debug.WriteLine("*************************************************************** Conexión a la base de datos establecida correctamente.");
-}
+    var user = context.User;
+    if (user.Identity != null && user.Identity.IsAuthenticated)
+    {
+        Debug.WriteLine($"********************************************Usuario autenticado: {user.Identity.Name}");
+    }
+    else
+    {
+        Debug.WriteLine("*****************************************************No autenticado");
+    }
+    await next();
+});
 
 app.Run();
